35b4c44e4d850bd01e8b50125a686b1c6857f24a
ShuheiOhno <inu.suika8@gmail.com>
2021-11-30 23:55:19 +0900

    Update Dashboard.jsx

---
+++ src/Dashboard/Dashboard.jsx
@@ -0,0 +1,20 @@
+function Dashboard() {
+    return (
+        <>
+        <section className="container">
+            <h3>color generator</h3>
+            <form>
+                <input type="text" value="" placeholder="#2e2c3d" className="" />
+                <button className="btn" type="submit" >
+                    submit
+                </button>
+            </form>
+        </section>
+        <section className="colors">
+            
+        </section>
+        </>
+    )
+}
+
+export default Dashboard;
