462e322761c2f3494d2f78774a689093c04ff906
ShuheiOhno <inu.suika8@gmail.com>
2021-11-30 23:58:32 +0900

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import Dashboard from "./Dashboard.jsx";
+import Dashboard from "./Dashboard/Dashboard";
+
 
 function App(){
     return (
         <>
+        test
+        <Dashboard></Dashboard>
         </>
     )
 
